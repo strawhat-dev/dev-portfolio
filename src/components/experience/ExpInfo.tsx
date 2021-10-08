c89739b7358cd297c76c99f7a14833cbfb2bc2bd
strawhat-dev <trankn4@gmail.com>
2021-10-08 15:31:15 -0700

    card flip fix test

--- src/components/experience/ExpInfo.tsx
+++ src/components/experience/ExpInfo.tsx
@@ -1,41 +1,47 @@
+import { Typography } from '@mui/material';
+
 export const UciInfo = () => (
-  <ul style={{ fontFamily: 'Zilla', fontSize: '1em', margin: '4% 10%' }}>
-    <li>
-      Tutor UCI students on general good practices and C++ language concepts
-      such as memory allocation, pointers, references, classes, linked lists,
-      and the standard library.
-    </li>
-    <li>Collaborate with professors on effective tutoring strategies.</li>
-    <li>
-      Develop strong communication skills and the ability to work well with
-      other people, while building a strong foundation in C++.
-    </li>
-  </ul>
+  <Typography fontFamily="Zilla" variant="body1" margin="4% 10%">
+    <ul>
+      <li>
+        Tutor UCI students on general good practices and C++ language concepts
+        such as memory allocation, pointers, references, classes, linked lists,
+        and the standard library.
+      </li>
+      <li>Collaborate with professors on effective tutoring strategies.</li>
+      <li>
+        Develop strong communication skills and the ability to work well with
+        other people, while building a strong foundation in C++.
+      </li>
+    </ul>
+  </Typography>
 );
 
 export const WalmartInfo = () => (
-  <ul style={{ fontFamily: 'Bogle', fontSize: '1em', margin: '4% 10%' }}>
-    <li>
-      Development and deployment of complete web application from scratch, as
-      part of Walmart’s transportation execution and visibility rewrite effort
-      to Spring Boot.
-    </li>
-    <li>
-      Application to play a critical role in the identification of EDI data
-      errors from transportation carriers, a vital part of Walmart’s operations,
-      and will be utilized by developers to quickly identify and process these
-      errors.
-    </li>
-    <li>
-      Implementation of backend using Spring Boot with connectivity to
-      Microsoft’s Azure Cosmos DB, to create REST API services designed to
-      reprocess data, with custom search functionality included.
-    </li>
-    <li>
-      Implementation of frontend using NodeJS, React, Next.js and Bootstrap to
-      provide a UI interface that will help present load transit status data in
-      a more human readable form and allow users and developers to search by
-      different fields.
-    </li>
-  </ul>
+  <Typography fontFamily="Bogle" variant="body1" margin="4% 10%">
+    <ul>
+      <li>
+        Development and deployment of complete web application from scratch, as
+        part of Walmart’s transportation execution and visibility rewrite effort
+        to Spring Boot.
+      </li>
+      <li>
+        Application to play a critical role in the identification of EDI data
+        errors from transportation carriers, a vital part of Walmart’s
+        operations, and will be utilized by developers to quickly identify and
+        process these errors.
+      </li>
+      <li>
+        Implementation of backend using Spring Boot with connectivity to
+        Microsoft’s Azure Cosmos DB, to create REST API services designed to
+        reprocess data, with custom search functionality included.
+      </li>
+      <li>
+        Implementation of frontend using NodeJS, React, Next.js and Bootstrap to
+        provide a UI interface that will help present load transit status data
+        in a more human readable form and allow users and developers to search
+        by different fields.
+      </li>
+    </ul>
+  </Typography>
 );
