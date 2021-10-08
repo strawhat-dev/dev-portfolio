faba455d7b6167364e7eb953c47e2aaefe328b0a
strawhat-dev <trankn4@gmail.com>
2021-10-08 14:35:45 -0700

    accessibility validation test

--- src/components/common/NewTabLink.tsx
+++ src/components/common/NewTabLink.tsx
@@ -9,6 +9,7 @@ export const NewTabLink = ({ href, content }: NewTabLink) => {
   return (
     <Link href={href} passHref>
       <a
+        title={href}
         target="_blank"
         rel="noopener noreferrer"
         style={{ maxWidth: 'fit-content' }}
