fb586b45a21f33b50c35b909c4a8234db4330e84
strawhat-dev <trankn4@gmail.com>
2021-10-09 15:23:04 -0700

    mobile fs

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,6 +8,8 @@ export const Header = () => {
       <meta name="author" content="strawhat-dev" />
       <meta name="description" content="responsive developer portfolio" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
