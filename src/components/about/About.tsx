b90f4561d03072a90562043f0144579f4e2c68cf
strawhat-dev <trankn4@gmail.com>
2022-02-04 14:09:31 -0800

    darth sidious

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import { Container, Box, Typography, Stack } from '@mui/material';
-import { ArrowRightAlt } from '@mui/icons-material/';
 import { Sphere } from '@/components/about/Sphere';
 import { animate } from '@/util/animate';
+import { ArrowRightAlt } from '@mui/icons-material/';
+import { Box, Container, Stack, Typography } from '@mui/material';
+import { useEffect } from 'react';
 
 const SwipeHeader = () => (
   <Box
@@ -62,16 +62,17 @@ export const About = ({
           </Typography>
         )}
         <Typography className="ani-right" variant="body1" marginBottom="4%">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Curabitur
-          vitae nunc sed velit dignissim sodales ut. Pulvinar mattis nunc sed
-          blandit libero volutpat. Consectetur libero id faucibus nisl tincidunt
-          eget nullam non nisi. Magna sit amet purus gravida quis blandit turpis
-          cursus in. Bibendum neque egestas congue quisque egestas. Adipiscing
-          elit pellentesque habitant morbi tristique senectus. Eget velit
-          aliquet sagittis id consectetur. Nec tincidunt praesent semper feugiat
-          nibh. Cras semper auctor neque vitae tempus quam. Dignissim enim sit
-          amet venenatis urna cursus eget nunc scelerisque.
+          Did you ever hear the tragedy of Darth Plagueis The Wise? I thought
+          not. It's not a story the Jedi would tell you. It's a Sith legend.
+          Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he
+          could use the Force to influence the midichlorians to create life… He
+          had such a knowledge of the dark side that he could even keep the ones
+          he cared about from dying. The dark side of the Force is a pathway to
+          many abilities some consider to be unnatural. He became so powerful
+          the only thing he was afraid of was losing his power, which
+          eventually, of course, he did. Unfortunately, he taught his apprentice
+          everything he knew, then his apprentice killed him in his sleep.
+          Ironic. He could save others from death, but not himself.
         </Typography>
       </Box>
       <Box className="ani" sx={{ width: isMobile ? '100%' : '40%' }}>
