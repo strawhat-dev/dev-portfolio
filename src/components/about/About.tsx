842e7f2e43c45435f41b2041abfb55790ad9e396
strawhat-dev <trankn4@gmail.com>
2022-02-04 14:13:02 -0800

    darth sidious

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -65,7 +65,7 @@ export const About = ({
           Did you ever hear the tragedy of Darth Plagueis The Wise? I thought
           not. It's not a story the Jedi would tell you. It's a Sith legend.
           Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he
-          could use the Force to influence the midichlorians to create life… He
+          could use the Force to influence the midichlorians to create life. He
           had such a knowledge of the dark side that he could even keep the ones
           he cared about from dying. The dark side of the Force is a pathway to
           many abilities some consider to be unnatural. He became so powerful
